5a011b4df26fb2f43bae8c41eca92585f75b721b
Paul Jackson <pj2454@gmail.com>
2014-05-15 11:15:36 -0600

    publisher back to what it should be

--- app/workers/publisher_worker.rb
+++ app/workers/publisher_worker.rb
@@ -2,13 +2,8 @@ class PublisherWorker
   def self.create_publishers(parsed_response)
     puts 'Creating Publishers...'
     parsed_response.each do |pub|
-      begin
-        puts "Publisher: #{pub[:mn_name]}"
-        Publisher.find_or_create_by(name: pub[:mn_name])
-      rescue Exception => e
-        puts e.message
-        puts e.backtrace.inspect
-      end
+      puts "Publisher: #{pub[:mn_name]}"
+      Publisher.find_or_create_by(name: pub[:mn_name])
     end
 
     CreatorWorker.create_creators(parsed_response)
