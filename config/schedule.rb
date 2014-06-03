c1e3565a885d5bbd563fa3645fa3283eae650c34
Paul Jackson <pj2454@gmail.com>
2014-06-02 19:17:54 -0600

    Whitespace errors

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,6 +8,6 @@ job_type :runner, %Q{export PATH=/Users/X-Wing/.rbenv/shims:/Users/X-Wing/src/.r
 
 set :output, "/Users/X-Wing/Desktop/cron_log.log"
 
-every :wednesday, at: '2:00am'   do
+every :wednesday, at: '2:00am' do
   runner 'ComicScraperWorker.perform_async', environment: 'development'
 end
