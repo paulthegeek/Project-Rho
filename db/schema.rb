1ed0cae2c1f0bfa8441fe42c90e5eab1830ebaa6
paulthegeek <pj2454@gmail.com>
2014-10-01 23:52:11 -0600

    Boolean values should be boolean and not null

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929223519) do
+ActiveRecord::Schema.define(version: 20141002054648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
