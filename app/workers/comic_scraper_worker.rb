c550767383eba6e5c5ca0335943f052e074ccebd
paulthegeek <pj2454@gmail.com>
2015-07-26 12:23:15 -0600

    Adding stuff

--- app/workers/comic_scraper_worker.rb
+++ app/workers/comic_scraper_worker.rb
@@ -5,20 +5,17 @@ class ComicScraperWorker
   include Sidekiq::Worker
 
   def perform
-    today = Date.today.to_s
-    # next_wednesday = 1.week.from_now.to_s
-    scrape_release_dates(today)
-    # scrape_release_dates(next_wednesday)
+    wednesday = Chronic.parse('next wednesday').strftime('%m/%d/%y')
+    scrape_release_dates(wednesday)
   end
 
   def scrape_release_dates(date)
-    puts 'We are starting'
+    puts 'We are starting....'
     # response = <<-END_OF_STRING
     #   "[{\tpr_id:\"1333023\",\tpr_ttle:\"Aron Warners Pariah Vol 2 #4\",\tcg_name:\"Comics\",\tchildcat:\"Dark Horse\",\tcg_id:\"61\",\tsu_id:\"16341\",\tmn_name:\"Dark Horse\",\tpr_lprice:\"3.39\",\tpr_price:\"3.99\",\tpr_discount:\"15\",\tpr_qty:\"8\",\tcart:\"0\",\tpr_parentid:\"1333023\",\tpr_simg:\"1\",\tsu_id:\"16341\",\tissubscribe:\"0\",\tartst:\"Brett Weldele\",\twrit:\"Aaron Warner - Phillip Gelatt\",},]"
     # END_OF_STRING
 
-    uri = 'http://www.midtowncomics.com/store/ajax_wr_online.asp?cat=61&wdate=5/28/14'
-    # response = HTTParty.post(uri + date)
+    uri = "http://www.midtowncomics.com/store/ajax_wr_online.asp?cat=61&wdate=#{date}"
     response = HTTParty.post(uri)
     response = response.parsed_response
     ComicScraperWorker.data_cleanup(response)
