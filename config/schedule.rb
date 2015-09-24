d51450ba19ab760013563844229bad2add1d70ba
paulthegeek <pj2454@gmail.com>
2015-09-24 08:48:30 -0600

    update cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,7 @@
 job_type :runner, %Q{export PATH=/Users/X-Wing/.rbenv/shims:/Users/X-Wing/src/.rbenv/bin:/usr/bin:$PATH; eval "$(rbenv init -)"; \
                          cd :path && bundle exec rails runner -e development ':task' :output }
 
-set :output, "/Users/X-Wing/Desktop/cron_log.log"
+set :output, 'log/cron_log.log'
 
 every :wednesday, at: '2:00am' do
   runner 'ComicScraperWorker.perform_async', environment: 'development'
