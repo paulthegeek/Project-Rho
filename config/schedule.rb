7868412904aa645a5d6001e2a4150723fc68ef26
paulthegeek <pj2454@gmail.com>
2015-09-23 19:29:32 -0600

    Update cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,10 +8,6 @@ job_type :runner, %Q{export PATH=/Users/X-Wing/.rbenv/shims:/Users/X-Wing/src/.r
 
 set :output, "/Users/X-Wing/Desktop/cron_log.log"
 
-# every :wednesday, at: '2:00am' do
-#   runner 'ComicScraperWorker.perform_async', environment: 'development'
-# end
-
-every 1.hour do
+every :wednesday, at: '2:00am' do
   runner 'ComicScraperWorker.perform_async', environment: 'development'
 end
